d5f02f3594ea1fe26850194db92f4c805a082113
beandob <brandon.heller0@gmail.com>
2020-08-05 00:39:24 -0700

    switching between regular view and "info view".
    buttons to swipe left/right.
    
    handling for different views by pushstate/popstate (also handles back button in brower correctly).

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,12 @@
-from flask import render_template, url_for
+from flask import render_template, redirect
 from . import main
 
 
 @main.route('/', methods=['GET'])
 def index():
     return render_template("main/index.html")
+
+
+@main.route('/info', methods=['GET'])
+def info():
+    return redirect('/')
--- app/static/js/carousel.js
+++ app/static/js/carousel.js
@@ -3,10 +3,11 @@
 class Carousel {
 
     constructor(element) {
-
         this.board = element
 
+        this.isInfoView = false
         // add first two cards programmatically
+        this.push()
         this.push()
 
         // handle gestures
@@ -47,7 +48,9 @@ class Carousel {
                 this.onTap(e)
             })
             this.hammer.on('pan', (e) => {
-                this.onPan(e)
+                if (!this.isInfoView) {
+                    this.onPan(e)
+                }
             })
 
         }
@@ -76,50 +79,125 @@ class Carousel {
         }, 100)
     }
 
+    expandInfo () {
+        carousel.isInfoView = true
+        // enable transform transition
+        this.topCard.style.transition = 'width 100ms ease-out'
+        this.topCard.style.transition = 'height 100ms ease-out'
+        this.topCard.style.transition = 'top 100ms ease-out'
+
+        // apply rotation around Y axis
+        //*note* need to change "height" and "top" attributes to line up image at top of the screen.
+        this.topCard.style.width = '100%'
+        this.topCard.style.height = '70%'
+        this.topCard.style.top = '35%' //puts the topCard at the top of the screen.
+        this.topCard.style.borderRadius = '0%'
+        // this.topCard.style.boxShadow = 'none'
+
+        //hide the next card.
+        this.nextCard.style.display='none'
+
+        //show button to go back to regular view
+        this.topCard.children[1].style.display = 'block'
+    }
+
+    shrinkInfo () {
+        carousel.isInfoView = false
+        // enable transform transition
+        this.topCard.style.transition = 'width 100ms ease-out'
+        this.topCard.style.transition = 'height 100ms ease-out'
+        this.topCard.style.transition = 'top 100ms ease-out'
+
+        // apply rotation around Y axis
+        //*note* need to change height and top to line up image at top of the screen.
+        this.topCard.style.width = '97%'
+        this.topCard.style.height = '80%'
+        this.topCard.style.top = '50%' //puts the topCard at the top of the screen.
+        this.topCard.style.borderRadius = '2%'
+        this.topCard.style.boxShadow = '0px 4px 4px 0px rgba(0, 0, 0, 0.1)'
+
+        //re-show the next card behind top card.
+        this.nextCard.style.display='block'
+
+        //hide button when back in regular view
+        this.topCard.children[1].style.display = 'none'
+    }
+
+    throwCard (posX, deg) {
+        this.topCard.style.transition = 'transform 300ms ease-out'
+        this.topCard.style.transform =
+            'translateX(' + posX + 'px) rotate(' + deg + 'deg)'
+
+        // wait transition end
+        setTimeout(() => {
+            // remove swiped card
+            this.board.removeChild(this.topCard)
+            // add new card
+            this.push()
+            // handle gestures on new top card
+            this.handle()
+            this.isInfoView = false
+        }, 1000)
+    }
+
+
     onTap(e) {
         let bounds = this.topCard.getBoundingClientRect()
         let currImageNum = parseInt(this.topCard.getAttribute('currImage'))
         let numImagesOnCard = parseInt(this.topCard.getAttribute('numImages'))
+        let cardCurrImageArrayIndex = parseInt(this.topCard.getAttribute('currImageArrayIndex'))
 
-        // get finger position on left/right of card
+        // get finger position.
+
+        //bottom 1/4 of screen is a bottom tap.
+        this.isTappingBottom =
+            (e.center.y - bounds.top) < this.topCard.clientHeight *.75 ? false : true
+
+        //split screen vertically in half for left/right taps.
         this.isTappingLeft =
-            (e.center.x - bounds.left) > this.topCard.clientWidth / 2 ? false : true
+            (e.center.x - bounds.left) > this.topCard.clientWidth * .5 ? false : true
 
-        //if tap left, decrement current img counter. if tap right, increment.
-        if( this.isTappingLeft) {
-            if (currImageNum === 1) {
-                this.jiggleImage(e)
-            }
-            else {
-                //track the position in the image list (for the indicator tabs/bullets at the top of the images)
-                this.topCard.setAttribute('currImage', parseInt(this.topCard.getAttribute('currImage')) - 1)
-
-                //set the opacity of the image tabs to indicate the currently selected image
-                this.topCard.children[0].children[currImageNum-1].style.opacity='0.2'
-                this.topCard.children[0].children[currImageNum-2].style.opacity='1'
-
-                //update background image.
-                this.topCard.style.backgroundImage = "url(" + images[currImageNum-2].src + ")"
-            }
-            console.log('tap left')
+        if (this.isTappingBottom && !this.isInfoView ) {
+            console.log('tap bottom')
+            window.history.pushState({url:'info', title:'info'}, 'info', 'info')
+            this.expandInfo(e)
         }
         else {
-            if (currImageNum === numImagesOnCard) {
-                this.jiggleImage(e)
+            //if tap left, decrement current img counter. if tap right, increment.
+            if (this.isTappingLeft) {
+                if (currImageNum === 1) {
+                    this.jiggleImage(e)
+                } else {
+                    //track the position in the image list (for the indicator tabs/bullets at the top of the images)
+                    this.topCard.setAttribute('currImage', parseInt(this.topCard.getAttribute('currImage')) - 1)
+
+                    //set the opacity of the image tabs to indicate the currently selected image
+                    this.topCard.children[0].children[currImageNum - 1].style.opacity = '0.2'
+                    this.topCard.children[0].children[currImageNum - 2].style.opacity = '1'
+
+                    //update background image.
+                    this.topCard.style.backgroundImage = "url(" + images[cardCurrImageArrayIndex][currImageNum - 2].src + ")"
+                }
+                console.log('tap left')
+            } else {
+                if (currImageNum === numImagesOnCard) {
+                    this.jiggleImage(e)
+                } else {
+                    /* check if the "shrink back to regular view" button was pressed */
+                    if (e.target.classList[0] != 'backToRegularViewButton') {
+                        //track the position in the image list (for the indicator tabs/bullets at the top of the images)
+                        this.topCard.setAttribute('currImage', parseInt(this.topCard.getAttribute('currImage')) + 1)
+
+                        //set the opacity of the image tabs to indicate the currently selected image
+                        this.topCard.children[0].children[currImageNum - 1].style.opacity = '0.2'
+                        this.topCard.children[0].children[currImageNum].style.opacity = '1'
+
+                        //update background image.
+                        this.topCard.style.backgroundImage = "url(" + images[cardCurrImageArrayIndex][currImageNum].src + ")"
+                    }
+                }
+                console.log('tap right')
             }
-            else {
-                //track the position in the image list (for the indicator tabs/bullets at the top of the images)
-                this.topCard.setAttribute('currImage', parseInt(this.topCard.getAttribute('currImage')) + 1)
-
-                //set the opacity of the image tabs to indicate the currently selected image
-                this.topCard.children[0].children[currImageNum-1].style.opacity='0.2'
-                this.topCard.children[0].children[currImageNum].style.opacity='1'
-
-                //update background image.
-                this.topCard.style.backgroundImage = "url(" + images[currImageNum].src + ")"
-
-            }
-            console.log('tap right')
         }
     }
 
@@ -177,7 +255,7 @@ class Carousel {
 
             this.isPanning = false
 
-            let successful = false
+            // let successful = false
 
             // set back transition properties
             this.topCard.style.transition = 'transform 200ms ease-out'
@@ -186,49 +264,48 @@ class Carousel {
             // check threshold and movement direction
             if (propX > 0.25 && e.direction === Hammer.DIRECTION_RIGHT) {
 
-                successful = true
+                //posX
+                this.throwCard(this.board.clientWidth, deg)
+                // successful = true
                 // get right border position
-                posX = this.board.clientWidth
+                // posX = this.board.clientWidth
 
             } else if (propX < -0.25 && e.direction === Hammer.DIRECTION_LEFT) {
 
-                successful = true
+                //posX
+                this.throwCard(-(this.board.clientWidth + this.topCard.clientWidth), deg)
+                // successful = true
                 // get left border position
-                posX = -(this.board.clientWidth + this.topCard.clientWidth)
-
-            } else if (propY < -0.25 && e.direction === Hammer.DIRECTION_UP) {
-
-                successful = true
-                // get top border position
-                posY = -(this.board.clientHeight + this.topCard.clientHeight)
-
+                // posX = -(this.board.clientWidth + this.topCard.clientWidth)
             }
-
-            if (successful) {
-
-                // throw card in the chosen direction
-                this.topCard.style.transform =
-                    'translateX(' + posX + 'px) translateY(' + posY + 'px) rotate(' + deg + 'deg)'
-
-                // wait transition end
-                setTimeout(() => {
-                    // remove swiped card
-                    this.board.removeChild(this.topCard)
-                    // add new card
-                    this.push()
-                    // handle gestures on new top card
-                    this.handle()
-                }, 200)
-
-            } else {
-
+            else {
                 // reset cards position and size
                 this.topCard.style.transform =
                     'translateX(-50%) translateY(-50%) rotate(0deg) rotateY(0deg) scale(1)'
                 if (this.nextCard) this.nextCard.style.transform =
                     'translateX(-50%) translateY(-50%) rotate(0deg) rotateY(0deg) scale(0.95)'
-
             }
+
+
+            // } else if (propY < -0.25 && e.direction === Hammer.DIRECTION_UP) {
+            //
+            //     successful = true
+            //     // get top border position
+            //     posY = -(this.board.clientHeight + this.topCard.clientHeight)
+            //
+            // }
+
+
+            // if (successful) {
+
+                // throw card in the chosen direction
+
+
+            // } else {
+
+
+
+            // }
 
         }
 
@@ -236,38 +313,50 @@ class Carousel {
 
     push() {
 
-            var imageUrls = ["https://picsum.photos/800/?random=" + Math.round(Math.random() * 1000000),
-                             "https://picsum.photos/800/?random=" + Math.round(Math.random() * 1000000),
-                             "https://picsum.photos/800/?random=" + Math.round(Math.random() * 1000000),
-                             "https://picsum.photos/800/?random=" + Math.round(Math.random() * 1000000),
-                             "https://picsum.photos/800/?random=" + Math.round(Math.random() * 1000000),
-                             "https://picsum.photos/800/?random=" + Math.round(Math.random() * 1000000),
-                             "https://picsum.photos/800/?random=" + Math.round(Math.random() * 1000000),
-                             "https://picsum.photos/800/?random=" + Math.round(Math.random() * 1000000),
-                             "https://picsum.photos/800/?random=" + Math.round(Math.random() * 1000000)]
+        /*
+            <div class="card">
+                <div class="imageNumIndicator">
+                    <div id="imageNumIndicatorTab_1"></div>
+                    ...
+                </div>
+                <div class="backToRegularViewButton">
+                </div>
+            </div>
 
-            function preload(imageUrls) {
-                for (var i = 0; i < imageUrls.length; i++) {
-                    images[i] = new Image()
-                    images[i].src = imageUrls[i]
-                }
+         */
+        var imageUrls = ["https://picsum.photos/1400/?random=" + Math.round(Math.random() * 1000000),
+                         "https://picsum.photos/1400/?random=" + Math.round(Math.random() * 1000000)]
+
+        function preload(imageUrls) {
+            for (var i = 0; i < imageUrls.length; i++) {
+                images[currImageArrayIndex][i] = new Image()
+                images[currImageArrayIndex][i].src = imageUrls[i]
             }
+        }
 
-            preload(imageUrls)
+        images[currImageArrayIndex] = new Array()
+        preload(imageUrls)
 
         let card = document.createElement('div')
 
         card.classList.add('card')
-
-        card.setAttribute('numImages', 9)
-
+        card.setAttribute('numImages', 2)
         card.setAttribute('currImage', 1)
+        card.setAttribute('currImageArrayIndex', currImageArrayIndex)
 
+        /* create current image # indicator */
         let imageNumIndicator = document.createElement('div')
-
         imageNumIndicator.classList.add('imageNumIndicator')
-
         card.appendChild(imageNumIndicator)
+
+        /* create button to shrink info view back to normal view (bottom right of images section) */
+        let backToRegularViewButton = document.createElement('div')
+        backToRegularViewButton.classList.add('backToRegularViewButton')
+        card.appendChild(backToRegularViewButton)
+        backToRegularViewButton.onclick = function () {
+            // from /info back to main page (caught by popstate listener on index.html, same thing that catches the back button)
+            history.back()
+        }
 
 
         let imageNumIndicatorTab
@@ -288,10 +377,14 @@ class Carousel {
 
         card.id = 'card1'
 
-        card.style.backgroundImage =  "url(" + images[0].src + ")"
+        card.style.backgroundImage =  "url(" + images[currImageArrayIndex][0].src + ")"
+        card.style.boxShadow = '0 20px 20px 10px rgba(0, 0, 0, .1) inset'
 
         this.board.insertBefore(card, this.board.firstChild)
 
+        currImageArrayIndex = (currImageArrayIndex === 0) ? 1 : 0
     }
+
+
 
 }
