19643bb04e8739788f6b156471155eb611662bfc
beandob <brandon.heller0@gmail.com>
2020-08-15 12:33:18 -0700

    added more pairing functionality.

--- app/main/views.py
+++ app/main/views.py
@@ -32,10 +32,12 @@ def info():
 @main.route('/create_user_if_not_exists', methods=['POST'])
 def create_user_if_not_exists():
     user = User.query.filter_by(session_id='session:' + session.sid).first()
-    user_pairing_code = user.user_pairing_code
+
     if not user:
         user_pairing_code = User.insert_user(session['user_pairing_code'],
                          session_id='session:' + session.sid)
+    else:
+        user_pairing_code = user.user_pairing_code
     resp = make_response(user_pairing_code, 204)
     return resp
 
@@ -45,12 +47,7 @@ def account():
     return redirect('/')
 
 
-# @main.route('/hello')
-# def publish_hello():
-#     sse.publish({"message": "Hello!"}, type='greeting')
-#     return "Message sent!"
-
-
+#user requested to pair with another via account page -> Pair
 @main.route('/pair_request', methods=['POST'])
 def pair_request():
     data = ast.literal_eval(request.data.decode("utf-8"))
@@ -63,8 +60,7 @@ def pair_request():
 
     if user_requesting and user_to_pair:
         sse.publish({"user_code_requesting": user_code_requesting}, type='pair_request', channel=user_to_pair.user_pairing_code)
-        # sse.publish({"user_code_requesting",  user_code_requesting}, type='greeting')
-        #match the users together and commit to db
+
         # user_requesting.user_id_matched = user_to_pair.id
         # user_to_pair.user_id_matched = user_requesting.id
         # db.session.commit()
@@ -74,14 +70,25 @@ def pair_request():
         resp = make_response('', 400)
         return resp
 
-# @main.route('/set/')
-# def set():
-#     random_str = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(4))
-#     session['user_pairing_code'] = random_str
-#     return 'ok'
-#
-#
-# @main.route('/get/')
-# def get():
-#     x = session.get('key', 'not set')
-#     return x
+
+#user receives request and hits "pair" button on the pair overlay screen.
+@main.route('/pair_accept', methods=['POST'])
+def pair_accept():
+    data = ast.literal_eval(request.data.decode("utf-8"))
+
+    user_pairing_code_1 = str(data.get('user_pairing_code_1'))
+    user_pairing_code_2 = str(data.get('user_pairing_code_2'))
+
+    user_1 = User.query.filter_by(user_pairing_code=user_pairing_code_1).first()
+    user_2 = User.query.filter_by(user_pairing_code=user_pairing_code_2).first()
+
+    if user_1 and user_2:
+        # match the users together and commit to db
+        user_1.user_id_matched = user_2.id
+        user_2.user_id_matched = user_1.id
+        db.session.commit()
+        resp = make_response('', 204)
+        return resp
+    else:
+        resp = make_response('', 400)
+        return resp
